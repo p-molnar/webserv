5e4c3075a935810b12aa2e9cca4ef2e4665a21e5
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-12-20 11:11:39 +0100

    Bring all exception throwing fn under one try

--- requirements/srcs/main.cpp
+++ requirements/srcs/main.cpp
@@ -1,37 +1,25 @@
-#include "../includes/WebServer.hpp"
+#include "WebServer.hpp"
 #include <iostream>
 #include <vector>
 #include <sys/socket.h>
 
 int main()
 {
-    ServerSocket serverSocket;
-    WebServer server;
     try
     {
+        ServerSocket serverSocket;
+        WebServer server;
         serverSocket.bindPort(8080);
         serverSocket.listenPort(10);
-        std::cout << "Server is listening on port 8080..." << std::endl;
         ClientSocket clientSocket = serverSocket.acceptConnection();
-        std::cout << "Connection accepted" << std::endl;
+        server.addFdToPollManager(serverSocket.getServerFd(), POLLIN);
+        server.pollClients();
     }
     catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
         return EXIT_FAILURE;
     }
-    // server.addFdToPollManager(serverSocket.getServerFd(), POLLIN);
-    // while (true)
-    // {
-    //     try {
-    //         server.pollClients();
-    //     }
-    //     catch (const std::runtime_error& e)
-    //     {
-    //         std::cerr << "Error occured during polling: " << e.what();
-    //         break ;
-    //     }
-    // }
 
     return EXIT_SUCCESS;
 }
