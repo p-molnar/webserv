c54f57eec4eadb1feb2257edc84c94b92f51d963
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-12-20 11:21:09 +0100

    Remove codam headers

--- requirements/includes/ClientSocket.hpp
+++ requirements/includes/ClientSocket.hpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   ClientSocket.hpp                                   :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: tklouwer <tklouwer@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/12/06 11:53:18 by tklouwer      #+#    #+#                 */
-/*   Updated: 2023/12/18 14:08:00 by tklouwer      ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef CLIENTSOCKET_HPP
 #define CLIENTSOCKET_HPP
 
@@ -26,19 +14,19 @@
 #include <dirent.h>
 #include <sys/select.h>
 
-class ClientSocket 
+class ClientSocket
 {
-    private:
-        int     _clientfd;
+private:
+    int _clientfd;
 
-    public:
-        ClientSocket(int newsockfd);
-        ~ClientSocket();
+public:
+    ClientSocket(int newsockfd);
+    ~ClientSocket();
 
-        int getClientFd() { return this->_clientfd; };
-        ssize_t pullMessage(char *buffer, size_t length);
-        ssize_t sendMessage(const char *buffer, size_t length);
-        void closeFd();
+    int getClientFd() { return this->_clientfd; };
+    ssize_t pullMessage(char *buffer, size_t length);
+    ssize_t sendMessage(const char *buffer, size_t length);
+    void closeFd();
 };
 
 #endif
