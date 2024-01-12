10f5d9b0bdb13f16fc11061bc71e6fb7472444e7
Bram Provoost <bprovoos@student.codam.nl>
2024-01-12 11:28:50 +0100

    loding config

--- requirements/srcs/Webserver.cpp
+++ requirements/srcs/Webserver.cpp
@@ -2,26 +2,52 @@
 #include <stdexcept>
 #include "ServerSocket.hpp"
 
-void    WebServer::addFdToPollManager(int fd, short events)
+WebServer::WebServer(){};
+
+void WebServer::loadConfig()
 {
-    _pollManager.addFd(fd, events);
+    // load parsed configuration file;
+
+    // tmp stuff
+    // server_configs[8000] = 10;
+    // server_configs[8001] = 10;
+    // server_configs[8002] = 10;
+    server_configs[8080] = 10;
+};
+
+void WebServer::startService()
+{
+    // The server configuration is identified by its port number
+    // i.e., there should be a std::map<port, ServerConfig> server_configs that allows us
+    // to get a server configuration based on its port number
+
+    // tmp data structure while the ServerConfig parsed class is missing
+    Log::logMsg("Server started");
+    std::map<int, int>::iterator it = server_configs.begin();
+    std::map<int, int>::iterator ite = server_configs.end();
+
+    while (it != ite)
+    {
+        int port = it->first;
+        int backlog = it->second;
+
+        ServerSocket *server_socket = new ServerSocket;
+        server_sockets.push_back(server_socket);
+
+        server_socket->createSocket();
+        server_socket->bindPort(port);
+        server_socket->listenPort(backlog);
+        poll_manager.addSocket(server_socket);
+        it++;
+    }
+    poll_manager.pollRequests();
 }
 
-// void    WebServer::pollClients() 
-// {
-//     auto fds = _pollManager.getPollFds();
-//     if (poll(fds.data(), fds.size(), -1) < 0)
-//     {
-//         throw std::runtime_error("poll() failed:" + std::string(strerror(errno)));
-//     }
-//     for (auto& pfd : fds)
-//     {
-//         if (pfd.revents & POLLIN)
-//         {
-//             if (pfd.fd == 8080)
-//             {
-//                // addFdToPollManager(serverSocket.acceptconnection(), POLLIN); // << NEED TO WORK ON ACCEPTING AND ADDING TO INCOMING CLIENTS TO VECTOR
-//             }
-//         }
-//     }
-// }
+WebServer::~WebServer()
+{
+    for (size_t i = 0; i < server_sockets.size(); i++)
+    {
+        delete server_sockets[i];
+    }
+    Log::logMsg("Server(s) shutdown");
+}
