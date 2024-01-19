a1756fc7e0c9d5a53b293c982b2fade276647a47
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2024-01-19 13:19:34 +0100

    Add consts

--- requirements/includes/consts.hpp
+++ requirements/includes/consts.hpp
@@ -1,10 +1,12 @@
 #ifndef CONSTS__HPP
 #define CONSTS__HPP
 
-#define CRLF std::string("\r\n")
-#define DBL_CRLF std::string("\r\n\r\n")
-#define SP std::string(" ")
-
-#define SP_ " "
+#define CRLF std::string("\r\n")		 // carriage return and new line
+#define DBL_CRLF std::string("\r\n\r\n") // double carriage return and new line
+#define SP std::string(" ")				 // single space separator
+#define SP_ " "							 // single space (C string);
+#define HEADER_SEP ": "					 // separator of an http header's key-val pair
+#define QSTR_SEP std::string("?")		 // query parameter separator
+#define QPARAM_SEP std::string("&")		 // query string separator
 
 #endif
