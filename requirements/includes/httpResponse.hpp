b9b23135c47bf0bd7d80ed223e54fa9a2a518b6b
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2024-01-19 13:50:21 +0100

    Add several constructors

--- requirements/includes/httpResponse.hpp
+++ requirements/includes/httpResponse.hpp
@@ -1,21 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   httpResponse.hpp                                   :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: tklouwer <tklouwer@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2024/01/17 10:22:04 by tklouwer      #+#    #+#                 */
-/*   Updated: 2024/01/17 11:29:06 by tklouwer      ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef HTTPRESPONSE__HPP
 #define HTTPRESPONSE__HPP
 
 #include "iostream"
-#include "httpRequest.hpp"
 #include <map>
+
+#include "httpRequest.hpp"
+#include "cgiExecutor.hpp"
 
 /*  HTTP Response
     -   HTTP VERSION    "HTTP/1.1"
@@ -26,24 +16,27 @@
                 "Content-Length"
                 "Connection"
     -   Response Body   content being returned
-    
+
  */
-class httpResponse 
+class httpResponse
 {
-    private:
-        std::string                         _httpVersion;
-        std::string                         _statusCode;
-        std::string                         _body;
-        std::map<std::string, std::string>  _headers;
+private:
+    std::string _httpVersion;
+    std::string _statusCode;
+    std::string _body;
+    std::map<std::string, std::string> _headers;
 
-    public:
-        httpResponse() : _httpVersion("HTTP/1.1") {};
+public:
+    httpResponse();
+    httpResponse(const httpResponse &obj);
+    httpResponse operator=(const httpResponse &obj);
+    ~httpResponse();
 
-        void        setStatusCode(const std::string& code);
-        void        setBody(const std::string& body);
-        void        setHeaders(const std::string& key, const std::string& value);
+    void setStatusCode(const std::string &code);
+    void setBody(const std::string &body);
+    void setHeaders(const std::string &key, const std::string &value);
 
-        std::string generateResponse(httpRequest& request);
+    static httpResponse generateResponse(httpRequest &request);
 };
 
 #endif
--- requirements/srcs/httpResponse.cpp
+++ requirements/srcs/httpResponse.cpp
@@ -6,29 +6,44 @@
 /*   By: tklouwer <tklouwer@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/17 10:53:12 by tklouwer      #+#    #+#                 */
-/*   Updated: 2024/01/17 13:27:54 by tklouwer      ########   odam.nl         */
+/*   Updated: 2024/01/19 13:27:53 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "httpResponse.hpp"
 
-void    httpResponse::setBody(const std::string& body)
+void httpResponse::setBody(const std::string &body)
 {
     _body = body;
     setHeaders("Content-Length", std::to_string(body.size()));
 }
 
-void    httpResponse::setHeaders(const std::string& key, const std::string& value)
+void httpResponse::setHeaders(const std::string &key, const std::string &value)
 {
     _headers[key] = value;
 }
 
-void    httpResponse::setStatusCode(const std::string& statusCode)
+void httpResponse::setStatusCode(const std::string &statusCode)
 {
     _statusCode = statusCode;
 }
 
-// std::string    httpResponse::generateResponse(httpRequest& request)
-// {
-    
-// }
+httpResponse httpResponse::generateResponse(httpRequest &request)
+{
+    cgiExecutor::executeCgi(request.getRequestLineComp("request_uri"));
+    return httpResponse();
+}
+
+httpResponse::httpResponse(const httpResponse &obj)
+{
+    (void)obj;
+}
+
+httpResponse httpResponse::operator=(const httpResponse &obj)
+{
+    (void)obj;
+    return *this;
+};
+
+httpResponse::httpResponse() : _httpVersion("HTTP/1.1"){};
+httpResponse::~httpResponse() {}
