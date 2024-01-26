43f4e68cfdfb3c2ca3859dc0112b1c895c374f71
Bram Provoost <bprovoos@student.codam.nl>
2024-01-26 11:45:08 +0100

    go to error page

--- requirements/srcs/FileHandler.cpp
+++ requirements/srcs/FileHandler.cpp
@@ -37,13 +37,14 @@ void     handleGetRequest(const httpRequest *req, httpResponse *res)
     Log::logMsg("Handling GET request");
     // std::string uri = req->getHeader("request_uri");
     // if (uri.empty() || uri == "/")
-    std::string uri = "/index.html";
+    std::string uri = "/index1.html";
+    std::string filePathNotFound = "www/error.html";
     
     std::string filePath = "www" + uri;
-    std::cout << "\n\n" << filePath << "\n\n" << std::endl;
     if (fileHandler::fileExists(filePath))
     {
         Log::logMsg("File exists");
+        std::cout << "\n\n" << filePath << "\n\n" << std::endl;
         std::string content = fileHandler::readFileContent(filePath);
         httpStatus status(statusCode::OK);
         res->setBody(content);
@@ -55,5 +56,26 @@ void     handleGetRequest(const httpRequest *req, httpResponse *res)
         }
         res->setHeaders("Content-Length", std::to_string(content.size()));
         res->generateResponse(true);
+    }
+    else if (fileHandler::fileExists(filePathNotFound))
+    {
+        Log::logMsg("File not found");
+        std::cout << "\n\n" << filePathNotFound << "\n\n" << std::endl;
+        std::string content = fileHandler::readFileContent(filePathNotFound);
+        httpStatus status(statusCode::NotFound);
+        res->setBody(content);
+        res->setStatusLine(status.getStatusLine());
+        const auto& headers = req->getHeaders();
+        for (const auto& header : headers)
+        {
+            res->setHeaders(header.first, header.second);
+        }
+        res->setHeaders("Content-Length", std::to_string(content.size()));
+        res->generateResponse(true);
+    }
+    else
+    {
+        Log::logMsg("Error file not found");
+        std::cout << "\n\n" << filePath << "\n\n" << std::endl;
     }
 }
