24392478473c721e2eb682b5f3a693644d47a6c8
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2024-01-26 17:36:14 +0100

    Fix raw form data parser

--- requirements/includes/FormData.hpp
+++ requirements/includes/FormData.hpp
@@ -26,14 +26,21 @@ typedef struct s_form_data
 
 class FormData
 {
-
 private:
 	std::string raw_data;
-	std::vector<t_form_data> form_data_arr;
+	std::vector<t_form_data> form_data;
 
 public:
+	FormData();
 	FormData(const std::string &data, const std::map<std::string, std::string> &headers);
+	FormData(const FormData &obj);
+	FormData operator=(const FormData &obj);
 	~FormData();
+
+	std::vector<std::string> tokenizeFormData(const std::string &data, std::string &form_id);
+
+public:
+	const std::vector<t_form_data> &getFormData() const;
 
 private:
 	t_form_overhead parseFormOverhead(const std::string &raw_overhead);
--- requirements/srcs/FormData.cpp
+++ requirements/srcs/FormData.cpp
@@ -1,46 +1,82 @@
 #include "FormData.hpp"
 
+FormData::FormData() {}
+
+FormData::FormData(const FormData &obj)
+	: raw_data(obj.raw_data),
+	  form_data(obj.form_data)
+{
+}
+
+FormData FormData::operator=(const FormData &obj)
+{
+	raw_data = obj.raw_data;
+	form_data = obj.form_data;
+	return *this;
+}
+
 t_form_overhead FormData::parseFormOverhead(const std::string &raw_overhead)
 {
-	t_form_overhead overhead_data;
-	overhead_data.raw_overhead = raw_overhead;
+	t_form_overhead overhead;
+	overhead.raw_overhead = raw_overhead;
 
-	std::vector<std::string> raw_headers = tokenize(raw_overhead, CRLF);
-	std::vector<std::string>::iterator it = raw_headers.begin();
-	std::vector<std::string>::iterator ite = raw_headers.end();
+	std::vector<std::string> raw_headers_comps = tokenize(overhead.raw_overhead, CRLF);
 
-	while (it != ite)
+	for (std::string raw_header : raw_headers_comps)
 	{
-		std::vector<std::string> header_comps = tokenize(*it, HEADER_SEP);
+		// std::cout << "|" << raw_header << "|\n";
+		std::vector<std::string> header_comps = tokenize(raw_header, HEADER_SEP);
 		if (header_comps.size() != 2)
 			throw std::runtime_error("overhead header parse error");
-
-		overhead_data.raw_headers[header_comps[0]] = header_comps[1];
-		it++;
+		overhead.raw_headers[header_comps[0]] = strip(header_comps[1], " ");
 	}
-	std::vector<std::string> content_disposition_comps = tokenize(overhead_data.raw_headers.at("Content-Disposition"), SEMICOL);
-	it = content_disposition_comps.begin();
-	ite = content_disposition_comps.end();
+	std::vector<std::string> content_disposition_comps = tokenize(overhead.raw_headers.at("Content-Disposition"), SEMICOL);
 
-	while (it != ite)
+	for (std::string content_disposition_val : content_disposition_comps)
 	{
-		std::vector<std::string> content_disposition_val_comps = tokenize(*it, "=");
+		std::vector<std::string> content_disposition_val_comps = tokenize(content_disposition_val, "=");
 		if (content_disposition_val_comps.size() == 1)
-			overhead_data.content_disposition["data_type"] = *it;
+			overhead.content_disposition["data_type"] = strip(content_disposition_val_comps[0], " ");
 		else if (content_disposition_val_comps.size() == 2)
-			overhead_data.content_disposition[content_disposition_val_comps[0]] = strip(*it, "\"");
+		{
+			std::string k = strip(content_disposition_val_comps[0], " ");
+			std::string v = strip(content_disposition_val_comps[1], "\"");
+			overhead.content_disposition[k] = v;
+		}
 		else
 			throw std::runtime_error("Content-Disposition parse error");
-		it++;
 	}
 
-	overhead_data.content_type = overhead_data.raw_headers.at("Content-Type");
-	return overhead_data;
+	overhead.content_type = overhead.raw_headers.at("Content-Type");
+	return overhead;
+}
+
+std::vector<std::string> FormData::tokenizeFormData(const std::string &data, std::string &form_boundary)
+{
+	std::string form_boundary_start = "--" + form_boundary;
+	std::string form_boundary_end = form_boundary_start + "--";
+
+	std::string raw_form_data = data.substr(0, data.find(form_boundary_end));
+	std::vector<std::string> form_data_comps = tokenize(raw_form_data, form_boundary_start);
+
+	std::vector<std::string>::iterator it;
+	for (it = form_data_comps.begin(); it != form_data_comps.end(); it++)
+	{
+		if ((*it).find("Content-Disposition") == NPOS)
+			it = form_data_comps.erase(it);
+	}
+	return form_data_comps;
+}
+
+const std::vector<t_form_data> &FormData::getFormData() const
+{
+	return form_data;
 }
 
 FormData::FormData(const std::string &data, const std::map<std::string, std::string> &headers)
 {
 	raw_data = data;
+	// std::cout << "raw form data: " << data << "\n";
 	std::string content_type_field_val = headers.at("Content-Type");
 	const std::string boundary_key = "boundary=";
 	std::size_t boundary_pos = content_type_field_val.find(boundary_key);
@@ -48,50 +84,36 @@ FormData::FormData(const std::string &data, const std::map<std::string, std::str
 	if (boundary_pos != NPOS)
 	{
 		std::string boundary = content_type_field_val.substr(boundary_pos + boundary_key.length());
-		std::vector<std::string> raw_data_comps = tokenize(data, boundary);
+		if (boundary.length() > 70)
+			throw std::runtime_error("form boundary too long: '" + boundary + "'");
+		std::vector<std::string> raw_form_data_comps = tokenizeFormData(data, boundary);
 
-		std::vector<std::string>::iterator it = raw_data_comps.begin();
-		std::vector<std::string>::iterator ite = raw_data_comps.end();
-
-		while (it != ite)
+		for (std::string raw_form_data : raw_form_data_comps)
 		{
-			if (*it != "--")
+			raw_form_data = strip(raw_form_data, WHTSPC);
+			std::vector<std::string> raw_form_comps = tokenize(raw_form_data, TWO_CRLF);
+			if (raw_form_comps.size() != 2)
+				throw std::runtime_error("form parse error\n");
+			// std::cout << "|" << raw_form_data << "|\n";
+
+			t_form_overhead overhead = parseFormOverhead(raw_form_comps[0]);
+			std::string payload = raw_form_comps[1];
+
+			std::cout << "raw_overhead: " << overhead.raw_overhead << "\n";
+
+			for (std::pair<std::string, std::string> rh : overhead.raw_headers)
 			{
-				std::cout << "it: " << *it << '\n';
-				std::vector<std::string> form_comps = tokenize(*it, DBL_CRLF);
-				if (form_comps.size() != 2)
-					throw std::runtime_error("form parse error");
-
-				// std::cout << "form comp0 |" << form_comps[0] << "|" << '\n';
-				// std::cout << "form comp1 " << form_comps[1] << '\n';
-
-				t_form_overhead overhead = parseFormOverhead(form_comps[0]);
-				std::string payload = form_comps[1];
-
-				std::cout << "raw_overhead: " << overhead.raw_overhead << "\n";
-				std::map<std::string, std::string>::iterator it2 = overhead.raw_headers.begin();
-				std::map<std::string, std::string>::iterator ite2 = overhead.raw_headers.end();
-
-				while (it2 != ite2)
-				{
-					std::cout << it2->first << ": " << it2->second << "\n";
-					it2++;
-				}
-
-				it2 = overhead.content_disposition.begin();
-				ite2 = overhead.content_disposition.end();
-				while (it2 != ite2)
-				{
-					std::cout << it2->first << ": " << it2->second << '\n';
-					it2++;
-				}
-				std::cout << "content_type: " << overhead.content_type << "\n";
-
-				std::cout << "payload: " << payload << '\n';
-
-				form_data_arr.push_back((t_form_data){overhead, payload});
+				std::cout << rh.first << ": " << rh.second << "\n";
 			}
-			it++;
+
+			for (std::pair<std::string, std::string> cd : overhead.content_disposition)
+			{
+				std::cout << cd.first << ": " << cd.second << "\n";
+			}
+			std::cout << "content_type: " << overhead.content_type << "\n\n";
+			// std::cout << "payload: " << payload << '\n';
+
+			form_data.push_back((t_form_data){overhead, payload});
 		}
 	}
 }
