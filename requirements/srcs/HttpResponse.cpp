11eb2bd969357b29ff0cd046ee15069320c255b6
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2024-01-28 23:04:03 +0100

    Add directory listing function call prototype

--- requirements/srcs/HttpResponse.cpp
+++ requirements/srcs/HttpResponse.cpp
@@ -21,7 +21,17 @@ HttpResponse HttpResponse::generateResponse(HttpRequest &request)
     // std::string cgi_raw_output = RequestProcessor::executeCgi(request.getUriComps());
     // std::cout << cgi_raw_output << "\n";
 
-    RequestProcessor::uploadFiles(request);
+    // RequestProcessor::uploadFiles(request.getFormDataObj());
+
+    // if request_type == DIRECTORY
+    {
+        std::string path = request.getUriComps().path;
+        // replace with config file's default directory from which the website should be served
+        path = path == "/" ? "srv/www/" : "srv/www/" + path;
+
+        std::string s = RequestProcessor::listDirectoryContent(path);
+        std::cout << s << '\n';
+    }
     return HttpResponse();
 }
 
