cfae683531bd95f0b6728fa4c1f2050cb35b817d
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2024-02-21 20:56:05 +0100

    Fix type

--- requirements/srcs/socket/ClientSocket.cpp
+++ requirements/srcs/socket/ClientSocket.cpp
@@ -2,7 +2,7 @@
 #include "consts.hpp"
 #include <iostream>
 
-ClientSocket::ClientSocket(int fd, std::shared_ptr<ServerBlock> config) : reque st(config), response()
+ClientSocket::ClientSocket(int fd, std::shared_ptr<ServerBlock> config) : request(config), response()
 {
     this->fd = fd;
     this->pfd = (t_pollfd){fd, POLLIN, 0};
